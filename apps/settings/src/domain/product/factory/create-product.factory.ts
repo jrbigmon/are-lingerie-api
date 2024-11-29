86ddeae33b28ee386a9ae003f554e3fc1f71b5b5
jrbigmon <juniorterriaga0@gmail.com>
2024-11-29 16:59:21 -0300

    refactor: created type input

--- apps/settings/src/domain/product/factory/create-product.factory.ts
+++ apps/settings/src/domain/product/factory/create-product.factory.ts
@@ -1,9 +1,11 @@
 import { generateUUID } from '../../../../../@shared/util/generateUUID';
 import { Lingerie, LingerieProps } from '../entity/lingerie/lingerie';
 
+export type CreateLingerieInput = Omit<LingerieProps, 'id'>;
+
 export function createProductFactory() {
   return {
-    createLingerie(input: Omit<LingerieProps, 'id'>): Lingerie {
+    createLingerie(input: CreateLingerieInput): Lingerie {
       const id = generateUUID();
       return new Lingerie({ id, ...input });
     },
