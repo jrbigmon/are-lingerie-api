74eb77b48c3cbc9f315e5d64b39ec5113ad625e8
jrbigmon <juniorterriaga0@gmail.com>
2024-11-29 16:35:38 -0300

    test: date range object value

---
+++ apps/settings/src/domain/bag/object-value/date-range.spec.ts
@@ -0,0 +1,44 @@
+import { InvalidAttribute } from '../../../../../@shared/error/invalid-attribute';
+import { DateRange } from './date-range';
+
+describe('DateRange', () => {
+  it('should be instantiate a date range', () => {
+    const dateRange = new DateRange({
+      dateOfReceipt: new Date('2022-01-01'),
+      deliveryDate: new Date('2022-01-15'),
+    });
+
+    expect(dateRange.getDateOfReceipt()).toBeInstanceOf(Date);
+    expect(dateRange.getDateOfReceipt().toISOString()).toBe(
+      '2022-01-01T00:00:00.000Z',
+    );
+
+    expect(dateRange.getDeliveryDate()).toBeInstanceOf(Date);
+    expect(dateRange.getDeliveryDate().toISOString()).toBe(
+      '2022-01-15T00:00:00.000Z',
+    );
+  });
+
+  it('should be not instantiate a date range when date of receipt is less then delivery date', () => {
+    let errors = undefined;
+
+    try {
+      new DateRange({
+        dateOfReceipt: new Date('2022-01-15'),
+        deliveryDate: new Date('2022-01-01'),
+      });
+    } catch (error) {
+      errors = error;
+    } finally {
+      expect(errors).toBeDefined();
+      expect(errors).toMatchObject([
+        new InvalidAttribute(
+          'dateOfReceipt',
+          '2022-01-15T00:00:00.000Z',
+          DateRange.name,
+          ['Date of receipt cannot be after delivery date'],
+        ),
+      ]);
+    }
+  });
+});
