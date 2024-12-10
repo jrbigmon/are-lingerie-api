304e867571b7ef74b0d93de4fc43aa97a028b3b5
jrbigmon <juniorterriaga0@gmail.com>
2024-12-10 19:57:51 -0300

    feat: docker compose and docker file

--- apps/settings/src/main.ts
+++ apps/settings/src/main.ts
@@ -3,6 +3,6 @@ import { SettingsModule } from './settings.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(SettingsModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
