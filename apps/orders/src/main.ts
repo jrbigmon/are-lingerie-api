8f0fb6bc409dcd4df8ce6e0eeaa8963a70ec3276
jrbigmon <juniorterriaga0@gmail.com>
2024-12-10 21:37:30 -0300

    feat: adjusting server port

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.ORDER_PORT || 3002;
 
   await app.listen(port).then(() => {
     console.log(`Orders running in port ${port}`);
--- apps/settings/src/main.ts
+++ apps/settings/src/main.ts
@@ -3,7 +3,7 @@ import { SettingsModule } from './settings.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(SettingsModule);
-  const port = process.env.PORT || 3001;
+  const port = process.env.SETTINGS_PORT || 3001;
 
   await app.listen(port).then(() => {
     console.log(`Settings running in port ${port}`);
