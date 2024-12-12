fa3f4735d647716ef32b95360bea9ea0bd9c0a56
jrbigmon <juniorterriaga0@gmail.com>
2024-12-12 18:19:26 -0300

    feat: product repository

---
+++ apps/settings/src/application/product/repository/product.repository.interface.ts
@@ -0,0 +1,16 @@
+import { RepositoryInterface } from '../../../../../@shared/repository/repository.interface';
+// import { LingerieSize } from '../../../domain/product/entity/lingerie/lingerie';
+import { Product } from '../../../domain/product/entity/product';
+
+export interface ProductRepositoryInterface
+  extends RepositoryInterface<Product> {
+  findById(id: string): Promise<Product | null>;
+  findAll(): Promise<Array<Product>>;
+  // findByType(type: string): Promise<Array<Product>>;
+  // findByTypeAndSize(type: string, size: LingerieSize): Promise<Array<Product>>;
+  // findByTypeAndSizeAndBarcode(
+  //   type: string,
+  //   size: LingerieSize,
+  //   barcode: string,
+  // ): Promise<Product | null>;
+}
