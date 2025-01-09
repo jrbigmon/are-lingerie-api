024e0d051419a6b61e1a36b45df659c094f8be51
jrbigmon <juniorterriaga0@gmail.com>
2025-01-09 16:33:09 -0300

    feat: init entity by model

--- apps/orders/utils/instantiate-entites.ts
+++ apps/orders/utils/instantiate-entites.ts
@@ -1,3 +1,17 @@
+import { CustomerModel } from '../src/application/customer/model/customer.model';
+import { Customer } from '../src/domain/customer/entity/customer';
+
 export const instantiateEntities = () => {
-  return {};
+  const initCustomer = (customer: CustomerModel): Customer => {
+    return new Customer({
+      id: customer.id,
+      fullName: customer.fullName,
+      email: customer.email,
+      phone: customer.phone,
+    });
+  };
+
+  return {
+    initCustomer,
+  };
 };
